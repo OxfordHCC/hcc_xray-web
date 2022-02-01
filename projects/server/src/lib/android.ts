b768ab1cf89e184b1c32646ee0e63e6d6a521ec1
treebirg <treebirg@gmail.com>
2022-02-01 20:48:48 +0000

    fix type of version parameter in android endpoint

--- projects/server/src/lib/android.ts
+++ projects/server/src/lib/android.ts
@@ -5,7 +5,7 @@ SELECT *
 FROM app_versions
 WHERE app=$1 AND version=$2
 `)
-export function getByAPKNameVersion(name: string, version?: string){
+export function getByAPKNameVersion(name: string, version: string){
 	return async (client: PoolClient) => {
 		const { rows } = await client.query(apkByNameVersionQuery, [name, version]);
 		return rows[0];
